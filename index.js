52ab26e0c1eab3ad1e936848572f5d1531ada81d
justinjyoo <justinjyoo@gmail.com>
2018-01-17 20:24:49 -0800

    Implement /vehicles/:id/engine route

--- index.js
+++ index.js
@@ -13,35 +13,58 @@ app.get('/', (req, res) => {
   res.status(200).send('hello world')
 })
 
-app.post('/vehicles/:id', (req, res) => {
-  axios.post('http://gmapi.azurewebsites.net/getVehicleInfoService',{
+app.get('/vehicles/:id', (req, res) => {
+  axios.post('http://gmapi.azurewebsites.net/getVehicleInfoService', {
       "id": req.params.id,
       "responseType": "JSON"
   }).then( (response) => {
     let gmResponseData = response.data.data;
-
-    const vehiclesSmartcarResponse = {
-      "vin": gmResponseData.vin.value,
-      "color": gmResponseData.color.value,
-      "doorCount": utility.getVehicleDoorCount(gmResponseData),
-      "driveTrain": gmResponseData.driveTrain.value
-    }
-
-    res.status(200).send(vehiclesSmartcarResponse)
+    let smartcarVehiclesInfoResponse = utility.constructVehiclesInfoResponseObject(gmResponseData)
+    res.status(200).send(smartcarVehiclesInfoResponse)
   })
 })
 
-app.post('/vehicles/:id/doors', (req, res) => {
-  axios.post('http://gmapi.azurewebsites.net/getSecurityStatusService',{
+app.get('/vehicles/:id/doors', (req, res) => {
+  axios.post('http://gmapi.azurewebsites.net/getSecurityStatusService', {
       "id": req.params.id,
       "responseType": "JSON"
   }).then( (response) => {
     let gmResponseData = response.data.data;
-
-    const getSecurityStatusSmartcarResponse =
-
-    res.status(200).send(getSecurityStatusSmartcarResponse)
+    let smartcarDoorSecResponse = utility.constructDoorSecResponseObject(gmResponseData)
+    res.status(200).send(smartcarDoorSecResponse)
   })
-
 })
 
+app.get('/vehicles/:id/fuel', (req, res) => {
+  axios.post('http://gmapi.azurewebsites.net/getEnergyService', {
+      "id": req.params.id,
+      "responseType": "JSON"
+  }).then( (response) => {
+    let gmResponseData = response.data.data;
+    let smartcarFuelResponse = utility.constructEnergyRangeObject(gmResponseData, 'fuel')
+    res.status(200).send(smartcarFuelResponse)
+  })
+})
+
+app.get('/vehicles/:id/battery', (req, res) => {
+  axios.post('http://gmapi.azurewebsites.net/getEnergyService', {
+      "id": req.params.id,
+      "responseType": "JSON"
+  }).then( (response) => {
+    let gmResponseData = response.data.data;
+    let smartcarBatteryResponse = utility.constructEnergyRangeObject(gmResponseData, 'battery')
+    res.status(200).send(smartcarBatteryResponse)
+  })
+})
+
+app.post('/vehicles/:id/engine', (req, res) => {
+  axios.post('http://gmapi.azurewebsites.net/actionEngineService', {
+      "id": req.params.id,
+      "command": utility.convertEngineActionType(req.query.action),
+      "responseType": "JSON"
+  }).then( (response) => {
+    let gmResponseData = response.data;
+    let smartcarEngineActionResponse = utility.constructEngineActionResponseObject(gmResponseData)
+    res.status(200).send(smartcarEngineActionResponse)
+  })
+})

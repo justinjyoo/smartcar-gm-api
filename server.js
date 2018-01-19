aa49f27581fe940eca9d540aa00aab0b1d13e377
justinjyoo <justinjyoo@gmail.com>
2018-01-18 23:07:05 -0800

    Clean up function arguments in server.js

--- server.js
+++ server.js
@@ -11,22 +11,22 @@ let config = { 'responseType': 'JSON' }
 // https://localhost:3000/
 app.listen(port, () => console.log('GM to Smartcar API is listening on port 3000!'))
 
-app.get('/', (req, res) => {
+app.get('/', ( req, res ) => {
   res.status(200).send('hello world')
 })
 
-app.get('/vehicles/:id', (req, res) => {
+app.get('/vehicles/:id', ( req, res ) => {
   instance.post('/getVehicleInfoService', Object.assign({ id: req.params.id }, config))
-  .then( (response) => {
+  .then( ( response ) => {
     let gmResponseData = response.data.data
     let smartcarVehiclesInfoResponse = convert.vehiclesInfoResponseObject(gmResponseData)
     res.status(200).send(smartcarVehiclesInfoResponse)
   })
 })
 
-app.get('/vehicles/:id/doors', (req, res) => {
+app.get('/vehicles/:id/doors', ( req, res ) => {
   instance.post('/getSecurityStatusService', Object.assign({ id: req.params.id }, config))
-  .then( (response) => {
+  .then( ( response ) => {
     let gmResponseData = response.data.data
     let smartcarDoorSecResponse = convert.doorSecResponseObject(gmResponseData)
     res.status(200).send(smartcarDoorSecResponse)
@@ -35,28 +35,28 @@ app.get('/vehicles/:id/doors', (req, res) => {
 
 app.get('/vehicles/:id/fuel', (req, res) => {
   instance.post('/getEnergyService', Object.assign({ id: req.params.id }, config))
-  .then( (response) => {
+  .then( ( response ) => {
     let gmResponseData = response.data.data
     let smartcarFuelResponse = convert.energyRangeObject(gmResponseData, 'fuel')
     res.status(200).send(smartcarFuelResponse)
   })
 })
 
-app.get('/vehicles/:id/battery', (req, res) => {
+app.get('/vehicles/:id/battery', ( req, res ) => {
   instance.post('/getEnergyService', Object.assign({ id: req.params.id }, config))
-  .then( (response) => {
+  .then( ( response ) => {
     let gmResponseData = response.data.data
     let smartcarBatteryResponse = convert.energyRangeObject(gmResponseData, 'battery')
     res.status(200).send(smartcarBatteryResponse)
   })
 })
 
-app.post('/vehicles/:id/engine', (req, res) => {
+app.post('/vehicles/:id/engine', ( req, res ) => {
   instance.post('/actionEngineService', Object.assign({
     id: req.params.id,
     command: convert.convertEngineActionType(req.query.action)
   }, config))
-  .then( (response) => {
+  .then( ( response ) => {
     let gmResponseData = response.data
     let smartcarEngineActionResponse = convert.engineActionResponseObject(gmResponseData)
     res.status(200).send(smartcarEngineActionResponse)

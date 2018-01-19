91c83c611f85f308120d361d1c582dd0dced2b8c
justinjyoo <justinjyoo@gmail.com>
2018-01-19 10:11:44 -0800

    Reformat object in mockSmartcarResponses

--- spec/vehicleInfoRouteSpec.js
+++ spec/vehicleInfoRouteSpec.js
@@ -1,10 +1,19 @@
 const expect = require('chai').expect;
+const sinon = require('sinon')
+const sandbox = sinon.createSandbox();
+
 const request = require('request');
-const { stub } = require('sinon')
 
 process.env.NODE_ENV = 'test'
 
+
+
 describe('/vehicles/:id route', function() {
+
+  before( () => {
+
+  })
+
   it('should respond to GET requests to /vehicles/:id with a 400 status code when vehicle id parameter is missing', function(done) {
     request.get('http://127.0.0.1:3000/vehicles/1234', function(error, response, body) {
       expect(response.statusCode).to.equal(200);

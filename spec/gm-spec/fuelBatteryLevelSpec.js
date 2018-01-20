5b5df2fa0c3bb3ae4fb40a6a6451619e82ab0abd
justinjyoo <justinjyoo@gmail.com>
2018-01-19 16:00:47 -0800

    Implement tests that query GM API and ensure that all nested data is as expected

---
+++ spec/gm-spec/fuelBatteryLevelSpec.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect
+const axios = require('axios')
+const instance = axios.create({
+  baseURL: 'http://gmapi.azurewebsites.net',
+  // if request to GM takes longer than 5 seconds the request will be aborted
+  timeout: 5000
+})
+
+const config = { 'responseType': 'JSON' }
+
+// These tests will ping the GM API and SHOULD BE RUN SPARINGLY!
+
+describe('POST /getEnergyService', function() {
+
+  it('should respond with an expected nested response.data.data structure', () => {
+    const vehicleID = '1234'
+    const route = '/getEnergyService'
+
+    return instance.post( route, Object.assign({ id:  vehicleID }, config))
+    .then( res => {
+      const expectedKeys = ['tankLevel', 'batteryLevel']
+      expect(res).to.have.nested.property('data.data')
+      expect(res.data.data).to.have.all.keys(expectedKeys)
+    })
+    .catch( err => {
+      expect.fail(err.actual, err.expected, err.message);
+    })
+
+  })
+
+});
